a8aa3910c7c7f7923c7f965000ab847f2eeee594
Mateusz Midor <3demaniac@gmail.com>
2021-03-10 14:27:37 +0100

    Update to review comments

--- kentikapi/internal/api_endpoints/query.go
+++ kentikapi/internal/api_endpoints/query.go
@@ -1,21 +1,17 @@
 package api_endpoints
 
-// QuerySQL runs SQL command against all configured devices. Returns top X datasets.
 func QuerySQL() string {
 	return "/query/sql"
 }
 
-// QueryURL returns URL which a logged in user can use to directly access this query in Data Explorer in the Kentik Detect portal.
 func QueryURL() string {
 	return "/query/url"
 }
 
-// QueryData returns results in a JSON results array
 func QueryData() string {
 	return "/query/topXdata"
 }
 
-// QueryChart returns result as a chart image
 func QueryChart() string {
 	return "/query/topXchart"
 }
