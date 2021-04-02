5a1013df87d67b5d6a8b04ae0abe39379e67bab8
Mateusz Midor <3demaniac@gmail.com>
2021-04-02 09:33:26 +0200

    Complemented descriptions in schema and regenerated docs

--- terraform-provider-kentik-cloudexport/internal/provider/provider.go
+++ terraform-provider-kentik-cloudexport/internal/provider/provider.go
@@ -32,17 +32,20 @@ func New(version string) func() *schema.Provider {
 					Type:        schema.TypeString,
 					Optional:    true,
 					DefaultFunc: schema.EnvDefaultFunc("KTAPI_URL", nil),
+					Description: "Custom apiserver url can be specified either by apiurl attribute or KTAPI_URL environment variable. If not specified, default of <https://cloudexports.api.kentik.com> will be used",
 				},
 				"email": &schema.Schema{
 					Type:        schema.TypeString,
 					Required:    true,
 					DefaultFunc: schema.EnvDefaultFunc("KTAPI_AUTH_EMAIL", nil),
+					Description: "Authorization. Either email attribute or KTAPI_AUTH_EMAIL environment variable is required",
 				},
 				"token": &schema.Schema{
 					Type:        schema.TypeString,
 					Sensitive:   true,
 					Required:    true,
 					DefaultFunc: schema.EnvDefaultFunc("KTAPI_AUTH_TOKEN", nil),
+					Description: "Authorization. Either token attribute or KTAPI_AUTH_TOKEN  environment variable is required",
 				},
 			},
 			DataSourcesMap: map[string]*schema.Resource{
