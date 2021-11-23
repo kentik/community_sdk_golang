8f7a0d3872191912794ff7377cdcda9860c45707
maciej palczynski <palczynski.maciej@gmail.com>
2021-11-23 12:37:09 +0100

    Added examples in gRPC for Synthetics Services

--- examples/synthetics_example_test.go
+++ examples/synthetics_example_test.go
@@ -6,6 +6,9 @@ package examples
 import (
 	"context"
 	"fmt"
+	syntheticspb "github.com/kentik/api-schema-public/gen/go/kentik/synthetics/v202101beta1"
+	"google.golang.org/protobuf/types/known/fieldmaskpb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 	"testing"
 	"time"
 
@@ -61,14 +64,14 @@ func runDataServiceExamples() error {
 	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Second)
 	defer cancel()
 
-	// NOTE: test of id 3336 must exist
-	if err = runGetHealthForTests(ctx, client, []string{"3336"}); err != nil {
+	// NOTE: test of id 3541 must exist
+	if err = runGetHealthForTests(ctx, client, []string{"3541"}); err != nil {
 		fmt.Println(err)
 		return err
 	}
 
-	// NOTE: test of id 3336 must exist
-	if err = runGetTraceForTest(ctx, client, "3336"); err != nil {
+	// NOTE: test of id 3541 must exist
+	if err = runGetTraceForTest(ctx, client, "3541"); err != nil {
 		fmt.Println(err)
 		return err
 	}
@@ -325,7 +328,7 @@ func runListAgents(ctx context.Context, client *kentikapi.Client) error {
 // prepare a Test for sending in CREATE request
 func makeExampleTest() *synthetics.V202101beta1Test {
 	hostname := synthetics.NewV202101beta1HostnameTest()
-	hostname.SetTarget("dummy-ht")
+	hostname.SetTarget("dummy-ht.com")
 
 	ping := *synthetics.NewV202101beta1TestPingSettings()
 	ping.SetPeriod(60)
@@ -393,6 +396,378 @@ func makeExampleTest() *synthetics.V202101beta1Test {
 	test.SetExpiresOn(time.Now().Add(time.Hour * 6))
 	test.SetCdate(time.Now())
 	test.SetCreatedBy(user)
+
+	return test
+}
+
+func TestSyntheticsAPIGRPCExample(t *testing.T) {
+	assert.NoError(t, runGRPCAdminServiceExamples())
+	assert.NoError(t, runGRPCDataServiceExamples())
+}
+
+func runGRPCAdminServiceExamples() error {
+	client, err := NewClient()
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Second)
+	defer cancel()
+
+	if err = runGRPCCRUDTest(ctx, client); err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	if err = runGRPCListTests(ctx, client); err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	if err = runGRPCCRUDAgent(ctx, client); err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	if err = runGRPCListAgents(ctx, client); err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func runGRPCDataServiceExamples() error {
+	client, err := NewClient()
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Second)
+	defer cancel()
+
+	// NOTE: test of id 3541 must exist
+	if err = runGRPCGetHealthForTests(ctx, client, []string{"3541"}); err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	// NOTE: test of id 3541 must exist
+	if err = runGRPCGetTraceForTest(ctx, client, "3541"); err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func runGRPCCRUDTest(ctx context.Context, client *kentikapi.Client) error {
+	fmt.Println("### CREATE TEST")
+	test := makeGRPCExampleTest()
+	createReqPayload := &syntheticspb.CreateTestRequest{Test: test}
+	createResp, err := client.SyntheticsAdmin.CreateTest(ctx, createReqPayload)
+	if err != nil {
+		return err
+	}
+
+	PrettyPrint(createResp.Test)
+	fmt.Println("Created test")
+	testID := createResp.Test.Id
+
+	fmt.Println("### SET TEST STATUS")
+	setStatusReqPayload := &syntheticspb.SetTestStatusRequest{
+		Id:     testID,
+		Status: syntheticspb.TestStatus_TEST_STATUS_PAUSED,
+	}
+
+	_, err = client.SyntheticsAdmin.SetTestStatus(ctx, setStatusReqPayload)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Success")
+	fmt.Println()
+
+	fmt.Println("### GET TEST")
+	getReqPayLoad := &syntheticspb.GetTestRequest{Id: testID}
+	getResp, err := client.SyntheticsAdmin.GetTest(ctx, getReqPayLoad)
+	if err != nil {
+		return err
+	}
+	PrettyPrint(getResp.GetTest())
+	fmt.Println()
+
+	test = getResp.Test
+
+	fmt.Println("### PATCH TEST")
+	test.Name = "example-test-1 UPDATED"
+	patchReqPayload := &syntheticspb.PatchTestRequest{
+		Test: test,
+		Mask: &fieldmaskpb.FieldMask{Paths: []string{"test.name"}},
+	}
+
+	patchResp, err := client.SyntheticsAdmin.PatchTest(ctx, patchReqPayload)
+	if err != nil {
+		return err
+	}
+	PrettyPrint(patchResp.GetTest())
+	fmt.Println()
+
+	fmt.Println("### DELETE TEST")
+	deleteReqPayLoad := &syntheticspb.DeleteTestRequest{Id: testID}
+	_, err = client.SyntheticsAdmin.DeleteTest(ctx, deleteReqPayLoad)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Success")
+	fmt.Println()
+
+	return nil
+}
+
+func runGRPCListTests(ctx context.Context, client *kentikapi.Client) error {
+	fmt.Println("### LIST TESTS")
+
+	getAllResp, err := client.SyntheticsAdmin.ListTests(ctx, &syntheticspb.ListTestsRequest{})
+	if err != nil {
+		return err
+	}
+
+	if getAllResp.Tests != nil {
+		tests := getAllResp.GetTests()
+		fmt.Println("Num tests:", len(tests))
+		if getAllResp.InvalidTestsCount != 0 {
+			fmt.Println("Num invalid tests:", getAllResp.InvalidTestsCount)
+		}
+		PrettyPrint(tests)
+	} else {
+		fmt.Println("[no tests received]")
+	}
+	fmt.Println()
+
+	return nil
+}
+
+func runGRPCGetHealthForTests(ctx context.Context, client *kentikapi.Client, testIDs []string) error {
+	fmt.Println("### GET HEALTH FOR TESTS")
+
+	healthPayLoad := &syntheticspb.GetHealthForTestsRequest{
+		Ids: testIDs,
+		StartTime: timestamppb.New(time.Now().Add(-time.Hour)),
+		EndTime: timestamppb.Now(),
+	}
+
+	getHealthResp, err := client.SyntheticsData.GetHealthForTests(ctx, healthPayLoad)
+	if err != nil {
+		return err
+	}
+
+	if getHealthResp.Health != nil {
+		healthItems := getHealthResp.GetHealth()
+		fmt.Println("Num health items:", len(healthItems))
+		PrettyPrint(healthItems)
+	} else {
+		fmt.Println("[no health items received]")
+	}
+	fmt.Println()
+
+	return nil
+}
+
+func runGRPCGetTraceForTest(ctx context.Context, client *kentikapi.Client, testID string) error {
+	fmt.Println("### GET TRACE FOR TEST")
+
+	tracePayLoad := &syntheticspb.GetTraceForTestRequest{
+		Id:        testID,
+		StartTime: timestamppb.New(time.Now().Add(-time.Hour)),
+		EndTime:   timestamppb.Now(),
+	}
+
+	getTraceResp, err := client.SyntheticsData.GetTraceForTest(ctx, tracePayLoad)
+	if err != nil {
+		return err
+	}
+
+	if getTraceResp.Lookups != nil {
+		lookups := getTraceResp.Lookups
+		fmt.Println("Agents id by ip:")
+		PrettyPrint(lookups)
+	} else {
+		fmt.Println("[no agents received]")
+	}
+
+	if getTraceResp.TraceRoutes != nil {
+		results := getTraceResp.TraceRoutes
+		fmt.Println("Num trace routes:", len(results))
+		PrettyPrint(results)
+	} else {
+		fmt.Println("[no trace routes received]")
+	}
+
+	if getTraceResp.TraceRoutesInfo != nil {
+		results := getTraceResp.TraceRoutesInfo
+		fmt.Println("Trace routes info:")
+		PrettyPrint(results)
+	} else {
+		fmt.Println("[no trace routes info received]")
+	}
+
+	fmt.Println()
+
+	return nil
+}
+
+func runGRPCCRUDAgent(ctx context.Context, client *kentikapi.Client) error {
+	// NOTE: no CREATE method exists for agents in the API, thus no example for CREATE
+	// NOTE: agent of id 1717 must exist
+	agentID := "1717"
+
+	fmt.Println("### GET AGENT")
+	getReqPayLoad := &syntheticspb.GetAgentRequest{Id: agentID}
+	getResp, err := client.SyntheticsAdmin.GetAgent(ctx, getReqPayLoad)
+	if err != nil {
+		return err
+	}
+	PrettyPrint(getResp.GetAgent())
+	fmt.Println()
+
+	fmt.Println("### PATCH AGENT")
+	agent := getResp.GetAgent()
+	if agent.GetFamily() == syntheticspb.IPFamily_IP_FAMILY_V6 {
+		agent.Family = syntheticspb.IPFamily_IP_FAMILY_V4
+	} else {
+		agent.Family = syntheticspb.IPFamily_IP_FAMILY_V6
+	}
+	agent.Name = ""
+	patchReqPayload := &syntheticspb.PatchAgentRequest{
+		Agent: agent,
+		Mask:  &fieldmaskpb.FieldMask{Paths: []string{"agent.family"}},
+	}
+	patchResp, err := client.SyntheticsAdmin.PatchAgent(ctx, patchReqPayload)
+	if err != nil {
+		return err
+	}
+	PrettyPrint(patchResp.GetAgent())
+	fmt.Println()
+
+	// NOTE: as we can't create agents through the API - let's not delete them
+	//fmt.Println("### DELETE AGENT")
+	//deleteReqPayLoad := &syntheticspb.DeleteAgentRequest{Id: agentID}
+	//_, err = client.SyntheticsAdmin.DeleteAgent(ctx, deleteReqPayLoad)
+	//if err != nil {
+	//	return err
+	//}
+	//fmt.Println("Success")
+	//fmt.Println()
+
+	return nil
+}
+
+func runGRPCListAgents(ctx context.Context, client *kentikapi.Client) error {
+	fmt.Println("### LIST AGENTS")
+
+	getAllReq := &syntheticspb.ListAgentsRequest{}
+	getAllResp, err := client.SyntheticsAdmin.ListAgents(ctx, getAllReq)
+	if err != nil {
+		return err
+	}
+
+	if getAllResp.Agents != nil {
+		agents := getAllResp.GetAgents()
+		fmt.Println("Num agents:", len(agents))
+		if getAllResp.InvalidAgentsCount != 0 {
+			fmt.Println("Num invalid agents:", getAllResp.InvalidAgentsCount)
+		}
+		PrettyPrint(agents)
+	} else {
+		fmt.Println("[no agents received]")
+	}
+	fmt.Println()
+
+	return nil
+}
+
+func makeGRPCExampleTest() *syntheticspb.Test {
+	healthSettings := &syntheticspb.HealthSettings{
+		LatencyCritical:     0,
+		LatencyWarning:      0,
+		PacketLossCritical:  0,
+		PacketLossWarning:   0,
+		JitterCritical:      0,
+		JitterWarning:       0,
+		HttpLatencyCritical: 0,
+		HttpLatencyWarning:  0,
+		HttpValidCodes:      []uint32{},
+		DnsValidCodes:       []uint32{},
+	}
+
+	monitorSettings := &syntheticspb.TestMonitoringSettings{
+		ActivationGracePeriod: "2",
+		ActivationTimeUnit:    "m",
+		ActivationTimeWindow:  "5",
+		ActivationTimes:       "3",
+		NotificationChannels:  []string{},
+	}
+
+	ping := &syntheticspb.TestPingSettings{
+		Period: 60,
+		Count:  5,
+		Expiry: 3000,
+	}
+
+	trace := &syntheticspb.TestTraceSettings{
+		Period:   60,
+		Count:    3,
+		Protocol: "udp",
+		Port:     33434,
+		Expiry:   22500,
+		Limit:    30,
+	}
+
+	settings := &syntheticspb.TestSettings{
+		Definition: &syntheticspb.TestSettings_Hostname{
+			Hostname: &syntheticspb.HostnameTest{Target: "dummy-ht.com"},
+		},
+		AgentIds: []string{
+			"890",
+		},
+		Period: 0,
+		Count:  0,
+		Expiry: 0,
+		Limit:  0,
+		Tasks: []string{
+			"ping",
+			"traceroute",
+		},
+		HealthSettings:     healthSettings,
+		MonitoringSettings: monitorSettings,
+		Ping: ping,
+		Trace: trace,
+		Port:        443,
+		Protocol:    "icmp",
+		Family:      syntheticspb.IPFamily_IP_FAMILY_DUAL,
+		Servers:     []string{},
+		UseLocalIp:  false,
+		Reciprocal:  false,
+		RollupLevel: 1,
+		Http:        nil,
+	}
+
+	userInfo := &syntheticspb.UserInfo{
+		Id:       "144566",
+		Email:    "John Doe",
+		FullName: "john.doe@acme.com",
+	}
+
+	test := &syntheticspb.Test{
+		Name:          "example-test-1",
+		Type:          "hostname",
+		DeviceId:      "1000",
+		Status:        syntheticspb.TestStatus_TEST_STATUS_ACTIVE,
+		Settings:      settings,
+		ExpiresOn:     timestamppb.New(time.Now().Add(time.Hour*6)),
+		Cdate:         timestamppb.Now(),
+		CreatedBy:     userInfo,
+	}
 
 	return test
 }
--- examples/synthetics_mesh_example_test.go
+++ examples/synthetics_mesh_example_test.go
@@ -7,6 +7,8 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	syntheticspb "github.com/kentik/api-schema-public/gen/go/kentik/synthetics/v202101beta1"
+	"google.golang.org/protobuf/types/known/timestamppb"
 	"os"
 	"strconv"
 	"testing"
@@ -143,6 +145,139 @@ func newMetricsMatrix(mesh []synthetics.V202101beta1MeshResponse) metricsMatrix 
 }
 
 func (m metricsMatrix) getMetric(fromAgent string, toAgent string) (*synthetics.V202101beta1MeshMetrics, bool) {
+	toAgents, ok := m.cells[fromAgent]
+	if !ok {
+		return nil, false
+	}
+
+	metric, ok := toAgents[toAgent]
+	if !ok {
+		return nil, false
+	}
+
+	return metric, true
+}
+
+func TestGetMeshTestResultsGRPCExample(t *testing.T) {
+	assert.NoError(t, runGetMeshTestResultsGRPC())
+}
+
+func runGetMeshTestResultsGRPC() error {
+	flag.Parse()
+
+	mesh, err := getMeshTestResultsGRPC(*testID)
+	if mesh == nil {
+		fmt.Println("Empty mesh test result received")
+	} else {
+		metricsMatrix := newMetricsMatrixGRPC(*mesh)
+		printMetricsMatrixGRPC(metricsMatrix)
+	}
+
+	return err
+}
+
+func getMeshTestResultsGRPC(testID string) (*[]syntheticspb.MeshResponse, error) {
+	client, err := NewClient()
+	if err != nil {
+		return nil, err
+	}
+
+	healthPayload := &syntheticspb.GetHealthForTestsRequest{
+		Ids:       []string{testID},
+		StartTime: timestamppb.New(time.Now().Add(-time.Minute * 5)),
+		EndTime:   timestamppb.Now(),
+		Augment:   true, // if not set, returned Mesh pointer will be empty
+	}
+
+	getHealthResp, err := client.SyntheticsData.GetHealthForTests(context.Background(), healthPayload)
+	if err != nil {
+		return nil, err
+	}
+
+	if getHealthResp.Health != nil {
+		healthItems := getHealthResp.GetHealth()
+		fmt.Println("Num health items:", len(healthItems))
+		if len(healthItems) > 0 {
+			return healthItems[0].GetMesh(), nil
+		} else {
+			return nil, nil
+		}
+	} else {
+		fmt.Println("[no health items received]")
+		return nil, nil
+	}
+}
+
+func printMetricsMatrixGRPC(matrix metricsMatrix) {
+	w := makeTabWriter()
+
+	// print table header
+	header := ".\t"
+	for _, x := range matrix.agents {
+		header = header + x + "\t"
+	}
+	fmt.Fprintln(w, header)
+
+	// print table rows
+	for _, fromAgent := range matrix.agents {
+		row := fromAgent + "\t"
+		for _, toAgent := range matrix.agents {
+			if metrics, ok := matrix.getMetric(fromAgent, toAgent); ok {
+				row = row + formatLatency(metrics) + "\t"
+			} else {
+				row = row + "[X]\t"
+			}
+		}
+		fmt.Fprintln(w, row)
+	}
+
+	w.Flush()
+}
+
+func makeTabWriterGRPC() *tabwriter.Writer {
+	const minWidth = 0  // minimal cell width including any padding
+	const tabWidth = 2  // width of tab characters (equivalent number of spaces)
+	const padding = 4   // distance between cells
+	const padchar = ' ' // ASCII char used for padding
+	const flags = 0     // formatting control
+	w := tabwriter.NewWriter(os.Stdout, minWidth, tabWidth, padding, padchar, flags)
+	return w
+}
+
+func formatLatencyGRPC(metrics *syntheticspb.MeshMetrics) string {
+	latency, err := strconv.ParseInt(*metrics.GetLatency().Value, 10, 64)
+	if err != nil {
+		return "error"
+	}
+
+	return strconv.FormatInt(latency/1000, 10) + "ms" // latency is returned in thousands of milliseconds, so need to divide by 1000
+}
+
+// metricsMatrix holds "fromAgent" -> "toAgent" connection metrics
+type metricsMatrixGRPC struct {
+	agents []string
+	cells  map[string]map[string]*syntheticspb.MeshMetrics
+}
+
+func newMetricsMatrixGRPC(mesh []syntheticspb.MeshResponse) metricsMatrix {
+	// fill agents
+	agents := []string{}
+	for _, agent := range mesh {
+		agents = append(agents, agent.GetAlias())
+	}
+
+	// fill matrix cells
+	cells := make(map[string]map[string]*syntheticspb.MeshMetrics)
+	for _, fromAgent := range mesh {
+		cells[fromAgent.GetAlias()] = make(map[string]*syntheticspb.MeshMetrics)
+		for _, toAgent := range *fromAgent.Columns {
+			cells[fromAgent.GetAlias()][toAgent.GetAlias()] = toAgent.Metrics
+		}
+	}
+	return metricsMatrix{agents: agents, cells: cells}
+}
+
+func (m metricsMatrix) getMetricGRPC(fromAgent string, toAgent string) (*syntheticspb.MeshMetrics, bool) {
 	toAgents, ok := m.cells[fromAgent]
 	if !ok {
 		return nil, false
